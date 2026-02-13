fb0daadb5cccdbe482d253966869a9500fb57b48
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-12 23:16:01 -0600

    animation preview display in framesbar

--- pixel-art-studio/src/app/components/framesbar/framesbar.ts
+++ pixel-art-studio/src/app/components/framesbar/framesbar.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FrameModel } from '../../models/frame.model';
 import { ProjectService } from '../../services/project.service';
+import { AnimationPreview } from '../animation-preview/animation-preview';
 
 @Component({
   selector: 'app-framesbar',
-  imports: [],
+  imports: [AnimationPreview],
   templateUrl: './framesbar.html',
   styleUrl: './framesbar.scss',
 })
 export class Framesbar {
+  @ViewChild(AnimationPreview) animationPreview!: AnimationPreview;
+
   frames: FrameModel[] = [];
   activeFrameId: number | null = null;
+  isPlaying: boolean = false;
 
   constructor(private projectService: ProjectService) {
     this.projectService.project$.subscribe((project) => {
@@ -38,5 +42,10 @@ export class Framesbar {
     const input = event.target as HTMLInputElement;
     const nextDuration = Number(input.value);
     this.projectService.setFrameDuration(frameId, nextDuration);
+  }
+
+  togglePlaying(): void {
+    if (!this.animationPreview) return;
+    this.isPlaying = this.animationPreview.togglePlayback();
   }
 }
