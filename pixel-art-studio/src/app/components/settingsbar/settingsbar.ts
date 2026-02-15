ed1bea5c9b46f33f7c49bacc5a06293249fb0c34
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-14 23:11:58 -0600

    Implemented exporting project

--- pixel-art-studio/src/app/components/settingsbar/settingsbar.ts
+++ pixel-art-studio/src/app/components/settingsbar/settingsbar.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToolManagerService } from '../../services/tool-manager.service';
+import { ExportMode, ExportService } from '../../services/export.service';
 
 @Component({
   selector: 'app-settingsbar',
@@ -7,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './settingsbar.scss',
 })
 export class Settingsbar {
+  isExportPopupOpen: boolean = false;
 
+  constructor(
+    private toolManagerService: ToolManagerService,
+    private exportService: ExportService,
+  ) {}
+
+  saveProject(): void {
+    // Figure out logic after
+  }
+
+  openExportPopup(): void {
+    this.isExportPopupOpen = true;
+  }
+
+  closeExportPopup(): void {
+    this.isExportPopupOpen = false;
+  }
+
+  exportProject(mode: ExportMode): void {
+    this.exportService.exportProject(mode);
+    this.closeExportPopup();
+  }
+
+  undo(): void {
+    this.toolManagerService.handleUndo();
+  }
+
+  redo(): void {
+    this.toolManagerService.handleRedo();
+  }
 }
