303ac1368ac392387b36c6bbd0b5b13f9c8ff61c
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-16 21:47:32 -0600

    Interfaces for Auth

---
+++ pixel-art-studio/src/app/types/auth-interfaces/auth.ts
@@ -0,0 +1,17 @@
+import { User } from './user';
+
+export interface AuthResponse {
+  token: string;
+  user: User;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface SignUpData {
+  username: string;
+  email: string;
+  password: string;
+}
