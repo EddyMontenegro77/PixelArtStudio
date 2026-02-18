2c5b49d33b126f2c46a24b9f3fbb3bc53bc8bf28
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-17 19:50:48 -0600

    Updated auth routes with guards

--- pixel-art-studio/src/app/app.routes.ts
+++ pixel-art-studio/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth-guard';
+import { authGuard } from './guards/auth-guard';
 import { Login } from './components/auth/login/login';
 import { Signup } from './components/auth/signup/signup';
 import { Profile } from './pages/profile/profile';
+import { guestGuard } from './guards/guest-guard';
+import { Home } from './pages/home/home';
+import { Editor } from './pages/editor/editor';
 
 export const routes: Routes = [
-  { path: 'login', component: Login, canActivate: [AuthGuard] },
-  { path: 'signup', component: Signup, canActivate: [AuthGuard] },
-  { path: 'profile', component: Profile, canActivate: [AuthGuard] },
+  { path: '', component: Home },
+  { path: 'editor', component: Editor },
+  { path: 'login', component: Login, canActivate: [guestGuard] },
+  { path: 'signup', component: Signup, canActivate: [guestGuard] },
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
 ];
