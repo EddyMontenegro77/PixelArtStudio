c7713e6fdf134d3f079fa34d34fe3111cec85256
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-19 01:54:49 -0600

    home component basic impl

--- pixel-art-studio/src/app/pages/home/home.ts
+++ pixel-art-studio/src/app/pages/home/home.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  readonly isAuthenticated$;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.isAuthenticated$ = this.authService.isAuthenticated$;
+  }
+
+  goToEditor() {
+    this.router.navigate(['/editor']);
+  }
+
+  goToProfile() {
+    this.router.navigate(['/profile']);
+  }
+
+  goToLogIn() {
+    this.router.navigate(['/login']);
+  }
+
+  goToSignUp() {
+    this.router.navigate(['/signup']);
+  }
 }
