3e1653ada52d6693c6e3017c2f3e73f286a3a5f8
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-19 18:29:45 -0600

    Project card implementation

--- pixel-art-studio/src/app/components/project-card/project-card.ts
+++ pixel-art-studio/src/app/components/project-card/project-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './project-card.scss',
 })
 export class ProjectCard {
+  projectName = input.required<string>();
+  projectUuid = input.required<string>();
+  thumbnail = input<string | null>('/assets/placeholder_image.webp');
 
+  open = output<string>();
+  deleteRequested = output<string>();
+
+  handleOpenProject(): void {
+    this.open.emit(this.projectUuid());
+  }
+
+  handleDeleteProject(event: MouseEvent): void {
+    event.stopPropagation();
+    this.deleteRequested.emit(this.projectUuid());
+  }
 }
