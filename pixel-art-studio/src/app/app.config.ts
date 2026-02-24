c0eee8090ae376618739c7dab7c46cf36cb17bfc
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-23 19:26:42 -0600

    removed grid from import

--- pixel-art-studio/src/app/app.config.ts
+++ pixel-art-studio/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { Grid } from './components/grid/grid';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
