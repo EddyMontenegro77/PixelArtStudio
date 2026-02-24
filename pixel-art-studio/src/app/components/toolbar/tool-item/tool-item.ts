8b2bf5d3de4cca9008a019be939f9b26c28567b3
EddyMontenegro77 <edmon7719@gmail.com>
2026-02-24 01:09:09 -0600

    Active tool UI

--- pixel-art-studio/src/app/components/toolbar/tool-item/tool-item.ts
+++ pixel-art-studio/src/app/components/toolbar/tool-item/tool-item.ts
@@ -1,4 +1,5 @@
 import { Component, computed, input } from '@angular/core';
+import { ThemeService } from '../../../services/theme.service';
 
 @Component({
   selector: 'app-tool-item',
@@ -10,8 +11,16 @@ export class ToolItem {
   name = input.required<string>();
   keybind = input.required<string>();
   icon = input.required<string>();
+  isActive = input<boolean>(false);
+
+  constructor(private themeService: ThemeService) {}
 
   altIcon = computed(() => `${this.name()} icon`);
+  resolvedIcon = computed(() => {
+    const iconPath = this.icon().startsWith('/') ? this.icon() : `/${this.icon()}`;
+    const suffix = this.themeService.theme() === 'dark' ? 'white' : 'black';
+    return iconPath.replace(/_(black|white)\.svg$/, `_${suffix}.svg`);
+  });
 
   tooltipText = computed(() => {
     const name = this.name();
